21ae639f245f397bdf903de71b74f18ddbee59b9
aliad77 <aliaadallal21@gmail.com>
2025-09-26 00:10:50 +0300

    ch2

--- myApp.js
+++ myApp.js
@@ -1,8 +1,14 @@
-console.log("hello world");
 let express = require('express');
 let app = express();
 
+app.get('/', function(req,res){
 
+res.send("Hello Express");
+});
+
+app.listen(3000,function(){
+    console.log("server runing on port 3000");
+});
 
 
 
