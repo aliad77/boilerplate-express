5a7577ff2ea093404e6ad101a58c92cb06dc4bd7
aliad77 <aliaadallal21@gmail.com>
2025-09-28 17:37:20 +0300

    ch5

--- myApp.js
+++ myApp.js
@@ -1,11 +1,18 @@
 let express = require('express');
 let app = express();
 
+
+app.use('/public', express.static(__dirname + '/public'));
+
 app.get('/', function(req,res){
 
 res.sendFile(__dirname + '/views/index.html');
 });
-app.use('/public', express.static(__dirname + '/public'));
+
+
+app.get('/json', function(req, res) {
+  res.json({ message: "Hello json" });
+});
 
 app.listen(3000,function(){
     console.log("server runing on port 3000");
