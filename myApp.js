31215be26cd3abeffdc3a297fb9d1c512031cd4b
aliad77 <aliaadallal21@gmail.com>
2025-09-30 17:15:20 +0300

    6.15

--- myApp.js
+++ myApp.js
@@ -1,16 +1,22 @@
 require('dotenv').config();
+let express = require('express');
+let app = express();
 
-const express = require('express');
-const app = express();
+app.use('/public', express.static(__dirname + '/public'));
 
-app.get('/json', (req, res) => {
+app.get('/', function(req, res) {
+  res.sendFile(__dirname + '/views/index.html');
+});
+
+app.get("/json", function(req, res) {
   let message = "Hello json";
-
+  
   if (process.env.MESSAGE_STYLE === "uppercase") {
     message = message.toUpperCase();
   }
-
-  res.json({ message: message });
+  
+  res.json({"message": message});
 });
+
 
 module.exports = app;
