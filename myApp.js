5606a08b37f3b699671e30a9676d304afccd7212
aliad77 <aliaadallal21@gmail.com>
2025-09-30 12:17:38 +0300

    6.8

--- myApp.js
+++ myApp.js
@@ -1,23 +1,15 @@
 let express = require('express');
 let app = express();
 
-// Serve static assets from /public
-app.use('/public', express.static(__dirname + '/public'));
-
-// Serve HTML file at root
-app.get('/', function(req, res) {
-  res.sendFile(__dirname + '/views/index.html');
-});
-
-// Serve JSON at /json
 app.get('/json', function(req, res) {
   let response = { message: "Hello json" };
-  // Transform to uppercase only if MESSAGE_STYLE === "uppercase"
+  
+  // Transform to uppercase only if FCC sets MESSAGE_STYLE
   if (process.env.MESSAGE_STYLE === "uppercase") {
     response.message = response.message.toUpperCase();
   }
+
   res.json(response);
 });
 
-// Export the app for FCC testing
 module.exports = app;
