7e705e68ac6a4a1cc7d1f1ebf1667acf584ef9f6
aliad77 <aliaadallal21@gmail.com>
2025-09-30 16:11:03 +0300

    6.14

--- myApp.js
+++ myApp.js
@@ -1,21 +1,16 @@
-require('dotenv').config(); // Must be first
+require('dotenv').config();
 
 const express = require('express');
 const app = express();
 
 app.get('/json', (req, res) => {
-  // Default message
   let message = "Hello json";
 
-  // Read environment variable INSIDE route, exact match
   if (process.env.MESSAGE_STYLE === "uppercase") {
-    message = "HELLO JSON";
+    message = message.toUpperCase();
   }
 
-  // Send exact JSON
   res.json({ message: message });
 });
 
-app.listen(3000, () => console.log("Server running on port 3000"));
-
-module.exports = app; // FCC needs this
+module.exports = app;
