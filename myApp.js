d2fdef654bd31e3845854f6a04b934ed5ade00cc
aliad77 <aliaadallal21@gmail.com>
2025-09-30 21:22:31 +0300

    a

--- myApp.js
+++ myApp.js
@@ -26,9 +26,6 @@ app.get('/', function(req, res) {
   res.sendFile(filePath);
 });
 
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
-});
+
 
 module.exports = app;
