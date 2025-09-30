e9c8f37f8e28d114cb4403ed3edf9007ca7469d4
aliad77 <aliaadallal21@gmail.com>
2025-09-30 11:42:40 +0300

    ch6 updated

--- myApp.js
+++ myApp.js
@@ -1,12 +1,11 @@
+require('dotenv').config();
 let express = require('express');
 let app = express();
 
-require('dotenv').config();
 
 app.use('/public', express.static(__dirname + '/public'));
 
 app.get('/', function(req,res){
-
 res.sendFile(__dirname + '/views/index.html');
 });
 
