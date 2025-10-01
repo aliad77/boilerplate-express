81363444dcc7cc29ea93bbd29cfd5447a75c1659
aliad77 <aliaadallal21@gmail.com>
2025-10-01 13:53:36 +0300

    l

--- myApp.js
+++ myApp.js
@@ -2,28 +2,20 @@ require('dotenv').config();
 var express = require('express');
 var app = express();
 
-app.use('/public', express.static(__dirname + '/public'));
+app.use("/public", express.static(__dirname + "/public"));
 
-app.get('/', function(req, res) {
-  res.sendFile(__dirname + '/views/index.html');
+app.get("/", function(req, res) {
+  res.sendFile(__dirname + "/views/index.html");
 });
 
 app.get("/json", function(req, res) {
-  var response = {"message": "Hello json"};
+  var response = "Hello json";
   
-  if (process.env.MESSAGE_STYLE=== "uppercase") {
-    response.message = response.message.toUpperCase();
+  if (process.env.MESSAGE_STYLE === "uppercase") {
+    response = response.toUpperCase();
   }
   
-  res.json(response);
+  res.json({"message": response});
 });
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
-
-
-
-
 
 module.exports = app;
