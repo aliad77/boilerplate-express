74a1ac5e826a4bc65c356e254f569a91340fcc2f
aliad77 <aliaadallal21@gmail.com>
2025-10-01 14:22:56 +0300

    challenge6

--- myApp.js
+++ myApp.js
@@ -1,3 +1,4 @@
+require('dotenv').config();
 let express = require('express');
 let app = express();
 
@@ -12,12 +13,22 @@ app.get('/', function(req,res){
   res.sendFile(__dirname + '/views/index.html');
 });
 
-app.get('/json', function(req, res) {
+/*app.get('/json', function(req, res) {
   let response = { message: "Hello json" };
   if (process.env.MESSAGE_STYLE === "uppercase") {
     response.message = response.message.toUpperCase();
   }
   res.json(response);
+});
+*/
+app.get('/json', (req, res) => {
+  let message = "Hello json";
+
+  if (process.env.MESSAGE_STYLE === "uppercase") {
+    message = message.toUpperCase();
+  }
+
+  res.json({ message: message });
 });
 
 
