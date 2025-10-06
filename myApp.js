a9a19ca3ee31c6c3021f2e24683a21700c0dd27e
aliad77 <aliaadallal21@gmail.com>
2025-10-06 11:26:48 +0300

    challenge 9

--- myApp.js
+++ myApp.js
@@ -38,10 +38,10 @@ app.get('/now', (req, res, next) => {
   res.json({ time: req.time }); 
 });
 
-
-
-
-
+app.get('/:word/echo', (req, res) => {
+  const word = req.params.word; 
+  res.json({ echo: word });     
+});
 
 
 
